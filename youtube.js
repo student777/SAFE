a57bf374f330967a5a0a0f9e4c40df3579e9bb51
student777 <darkdakku@gmail.com>
2020-07-15 11:16:41 +0900

    update youtube

--- youtube.js
+++ youtube.js
@@ -3,9 +3,15 @@ var intervalID = setInterval(() => {
     if (!div || !div.innerHTML) return; // not loaded
     var skipBtn = document.querySelector('div.ytp-ad-text.ytp-ad-skip-button-text');
     skipBtn.click();
-    clearInterval(intervalID);
 }, 1000);
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 's')  document.querySelector('div.ytp-ad-text.ytp-ad-skip-button-text').click();
+setTimeout(() =>  {
+    clearInterval(intervalID);
+}, 5000)
+
+
+setTimeout(() =>{
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 's')  document.querySelector('div.ytp-ad-text.ytp-ad-skip-button-text').click();
+    });
 });
